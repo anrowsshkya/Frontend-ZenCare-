dc31b6eeb57b7662eddbe25e336975f37594ec48
Pratik <karkipratik063@gmail.com>
2025-03-27 20:19:06 +0545

    There are subfolders for each file. Appointment Form is not completed. Posted other code and working on appointment form

---
+++ components/Signup-Login/Login.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import google from "../../assets/google.png";
+import doctor from "../../assets/Doctor.jpg";
+import "./styles.css"; 
+import { loginUser } from "../api"; 
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); 
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    setError(""); 
+
+    try {
+      const response = await loginUser({ email, password }); 
+
+      if (response.status === 200) {
+        // Successful login: store tokens
+        localStorage.setItem("access_token", response.data.access);
+        localStorage.setItem("refresh_token", response.data.refresh);
+        alert(<span style={{color:'green'}}>Login Successfull!</span>);
+        navigate("/dashboard"); // Redirect to dashboard
+      }
+    } catch (err) {
+      // Handle API errors
+      setError(<span style={{color:'red'}}>{err.response?.data?.error || "Invalid credentials"}</span>);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="login-section">
+        <h2>Log in</h2>
+        <p>Log in with your data that you entered during registration</p>
+
+        {error && <p className="error-message">{error}</p>} 
+
+        <label htmlFor="email">Enter your email address</label>
+        <input type="email" id="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        <label htmlFor="password">Enter your password</label>
+        <input type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        <Link to="/forgotPassword" className="forgot-password">Forget Password?</Link>
+
+        {/* Add a navigation toward home page after clicking Log in */}
+        <button className="login-btn" onClick={handleLogin}>Log in</button>
+
+
+      {/* Button to Sign in with google */}
+        {/* <button className="google-btn"><img src={google} alt="Google Logo" />Sign in with Google</button> */}
+
+
+      {/* Will be naviagted to signup  */}
+        <p className="register-text">
+          Don't have an account? <a href="/SignUp">Register</a>
+        </p>
+      </div>
+
+      <div className="image-section">
+        <h3>Nice to see you again</h3>
+        <h1>Welcome back</h1>
+        <img src={doctor} alt="Illustration" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
